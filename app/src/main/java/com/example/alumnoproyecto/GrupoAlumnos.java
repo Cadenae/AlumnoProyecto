2f9d20f9de7f540dd473d311bdd2ddc5745ad071
Cadenae <cadenae@uabc.edu.mx>
2021-11-19 20:16:38 -0800

    intento serializable 12545

--- app/src/main/java/com/example/alumnoproyecto/GrupoAlumnos.java
+++ app/src/main/java/com/example/alumnoproyecto/GrupoAlumnos.java
@@ -1,5 +1,9 @@
 package com.example.alumnoproyecto;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class GrupoAlumnos {
@@ -8,6 +12,7 @@ public class GrupoAlumnos {
 
     private GrupoAlumnos(){
         listaAlumnos = new ArrayList<Alumno>();
+        Alumno alumno = new Alumno();
         for (int  i = 0 ; i<20; i++){
             Alumno alumno = new Alumno();
             alumno.setMatricula(i);
@@ -15,7 +20,17 @@ public class GrupoAlumnos {
             alumno.setActivo(i%2==0);
             listaAlumnos.add(alumno);
         }
+
+        try{
+            FileOutputStream fs = new FileOutputStream("Alumno.txt");
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(alumno.setMatricula(1));
+            os.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
+
 
     public static GrupoAlumnos getGrupo(){
         return grupo;
